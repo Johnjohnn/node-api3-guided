9e20b00faf2802ee18adb1af7cadf1a1757308fd
Johnjohnn <conde2010@aol.com>
2020-10-13 18:23:15 -0400

    adding stuff

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const morgan = require("morgan")                     // importing the npm install morgan add we just installed . 
 const welcomeRouter = require("./welcome/welcome-router")
 const usersRouter = require("./users/users-router")
 
@@ -6,6 +7,7 @@ const server = express()
 const port = 4000
 
 server.use(express.json())
+server.use(morgan("combined"))
 
 server.use(welcomeRouter)
 server.use(usersRouter)
